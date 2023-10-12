d188b405ae865f2ea8ced0709d9c9ff35934c929
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-10-12 18:23:16 -0300

    config: Configuração ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { ListModule } from './list/list.module';
-import { List } from './list/entities/list.entity';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { ListModule } from './list/list.module'
+import { List } from './list/entities/list.entity'
 
 @Module({
   imports: [
