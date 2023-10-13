100d6aa4e3098ce0a080bb09a891daa81120ac82
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-10-13 19:25:11 -0300

    UserEntity - constructor unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createdAt?: Date // ? -> Opcional
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date()
+  }
+}
