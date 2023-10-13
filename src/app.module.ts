5051213ea2955e5e2a4b5eec97e723207c8f4883
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-10-13 19:45:49 -0300

    UserEntity - getters methods unit tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { SequelizeModule } from '@nestjs/sequelize'
-import { ListModule } from './list/list.module'
-import { List } from './list/entities/list.entity'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 import { UsersModule } from './users/infrastructure/users.module'
 
 @Module({
-  imports: [
-    ListModule,
-    SequelizeModule.forRoot({
-      dialect: 'sqlite',
-      host: ':memory:',
-      autoLoadModels: true,
-      models: [List],
-    }),
-    EnvConfigModule,
-    UsersModule,
-  ],
+  imports: [EnvConfigModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
